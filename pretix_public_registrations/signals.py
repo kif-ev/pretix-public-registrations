92fb6828671066b7c1e6889bb88c9a41df481dcb
Felix Schäfer <felix@thegcat.net>
2019-06-13 23:51:24 +0200

    Add a public registration question to products

--- pretix_public_registrations/signals.py
+++ pretix_public_registrations/signals.py
@@ -1,1 +1,15 @@
-# Register your receivers here
+from django import forms
+from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
+from i18nfield.strings import LazyI18nString
+from pretix.presale.signals import question_form_fields
+
+
+@receiver(question_form_fields, dispatch_uid="pretix_public_registration_question")
+def add_public_registration_question(sender, **kwargs):
+    return {'public_registration': forms.CharField(
+        label=_('Public registration'),
+        required=False,
+        help_text=sender.settings.get('public_registration_field_help_text', as_type=LazyI18nString),
+        widget=forms.CheckboxInput(),
+    )}
