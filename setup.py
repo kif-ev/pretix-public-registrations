fc1bc2ff948b3bd1179265d093cf61f886f850ac
Felix Schäfer <felix@thegcat.net>
2019-11-13 22:34:51 +0100

    Better metadata for PyPI

--- setup.py
+++ setup.py
@@ -28,11 +28,22 @@ setup(
     version='1.3.1',
     description='This plugin will give the option to attendees of an event to mark their registration as public. Public registrations will be shown along their answers to questions marked as public by the organizers on a world-readable page.',
     long_description=long_description,
-    url='git@gitlab.fachschaften.org:kifev/pretix-public-registrations.git',
+    keywords="pretix public registrations",
+    url="https://gitlab.fachschaften.org/kif/pretix-public-registrations",
+    project_urls={
+        "Bug Tracker": 'https://gitlab.fachschaften.org/kif/pretix-public-registrations/issues',
+        "Source Code": 'https://gitlab.fachschaften.org/kif/pretix-public-registrations/tree/master',
+    },
     author='Felix Schäfer, Dominik Weitz',
     author_email='admin@kif.rocks',
     license='MIT License',
-
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Plugins",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
     install_requires=['django-gravatar2'],
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
