fb232e330733840e761500daf204b033927bd0d7
Felix Schäfer <felix@thegcat.net>
2020-04-25 23:28:02 +0200

    Release version 1.4.0

--- pretix_public_registrations/__init__.py
+++ pretix_public_registrations/__init__.py
@@ -14,7 +14,7 @@ class PluginApp(PluginConfig):
         author = 'Felix Schäfer, Dominik Weitz'
         description = _('This plugin will give the option to attendees of an event to mark their registration as public. Public registrations will be shown along their answers to questions marked as public by the organizers on a world-readable page.')
         visible = True
-        version = '1.3.2'
+        version = '1.4.0'
         compatibility = "pretix>=2.7.0"
 
     def ready(self):
