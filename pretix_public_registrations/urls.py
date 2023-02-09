f757302b0cb9275af9677e24247b80abc7e68c0b
Felix Schäfer <felix@thegcat.net>
2023-02-09 08:52:04 +0100

    Django 4 compatibility

--- pretix_public_registrations/urls.py
+++ pretix_public_registrations/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 
 from .views import PublicRegistrationsSettingsView
 
 urlpatterns = [
-    url(
-        r"^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/public_registrations/$",
+    path(
+        "control/event/<str:organizer>/<str:event>/public_registrations/",
         PublicRegistrationsSettingsView.as_view(),
         name="settings",
     )
